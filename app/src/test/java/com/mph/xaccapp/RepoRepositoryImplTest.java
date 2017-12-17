1b23d2908728cc30f7358c8be1c997b0f2029b57
Marcos Pueyo <marcosph0@gmail.com>
2017-12-17 21:19:08 +0100

    no message

--- app/src/test/java/com/mph/xaccapp/RepoRepositoryImplTest.java
+++ app/src/test/java/com/mph/xaccapp/RepoRepositoryImplTest.java
@@ -1,30 +1,50 @@
 package com.mph.xaccapp;
 
+import com.mph.xaccapp.domain.data.RepoRepository;
 import com.mph.xaccapp.domain.data.RepoRepositoryImpl;
+import com.mph.xaccapp.domain.data.model.Repository;
+import com.mph.xaccapp.domain.data.model.RepositoryDao;
 import com.mph.xaccapp.network.mapper.RestRepositoryMapper;
 import com.mph.xaccapp.network.service.RepositoryService;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
 
-import io.requery.Persistable;
-import io.requery.sql.EntityDataStore;
+import java.util.List;
 
 
 public final class RepoRepositoryImplTest {
 
     private RepositoryService mRepositoryService;
 
-    private EntityDataStore<Persistable> mDataStore;
+    private RepositoryDao mRepositoryDao;
 
     private RestRepositoryMapper mMapper;
 
-    private RepoRepositoryImpl repoRepositoryImpl;
+    private RepoRepositoryImpl mRepoRepositoryImpl;
 
     @Before
     public void setUp() throws Exception {
         mRepositoryService = Mockito.mock(RepositoryService.class);
-//        mDataStore = Mockito.mock()
+        mRepositoryDao = Mockito.mock(RepositoryDao.class);
+        mMapper = new RestRepositoryMapper();
+        mRepoRepositoryImpl = new RepoRepositoryImpl(mRepositoryService, mRepositoryDao, mMapper);
+    }
+
+    @Test
+    public void shouldLoadLocalRepos() throws Exception {
+        mRepoRepositoryImpl.getRepos(1, 10, new RepoRepository.GetReposListener() {
+            @Override
+            public void onReposLoaded(List<Repository> repositories) {
+
+            }
+
+            @Override
+            public void onDataNotAvailable() {
+
+            }
+        });
     }
 
 }
