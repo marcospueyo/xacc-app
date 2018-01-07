ae48d26f185c9be5d641a3ee959726b7dcee1db1
Marcos Pueyo <marcos.pueyo@est.fib.upc.edu>
2018-01-07 13:23:25 +0100

    - Added remote repo loading behaviour and data test.

--- app/src/test/java/com/mph/xaccapp/RepoRepositoryImplTest.java
+++ app/src/test/java/com/mph/xaccapp/RepoRepositoryImplTest.java
@@ -8,16 +8,22 @@ import com.mph.xaccapp.domain.data.model.Repository;
 import com.mph.xaccapp.domain.data.model.RepositoryDao;
 import com.mph.xaccapp.network.mapper.RestRepositoryMapper;
 import com.mph.xaccapp.network.service.RepositoryService;
+import com.mph.xaccapp.network.service.RepositoryServiceImpl;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Inject;
+
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -27,41 +33,81 @@ import static org.junit.Assert.*;
 
 public final class RepoRepositoryImplTest {
 
-    public static final String TAG = "RepoRepositoryImplTest";
-
-    private RepositoryService mRepositoryService;
-
     private RepositoryDao mRepositoryDao;
 
     private RestRepositoryMapper mMapper;
 
     private RepoRepositoryImpl mRepoRepositoryImpl;
 
+    private RepositoryService mRepositoryService;
+
+    private int page;
+
+    private int elementsPerPage;
+
+    private RepoRepository.GetReposListener listener;
+
     @Before
     public void setUp() throws Exception {
-        mRepositoryService = mock(RepositoryService.class);
         mRepositoryDao = mock(RepositoryDao.class);
+        mRepositoryService = mock(RepositoryService.class);
+        listener = mock(RepoRepository.GetReposListener.class);
         mMapper = new RestRepositoryMapper();
+
+        page = 1;
+        elementsPerPage = 10;
+
         mRepoRepositoryImpl = new RepoRepositoryImpl(mRepositoryService, mRepositoryDao, mMapper);
     }
 
     @Test
     public void shouldLoadLocalRepos() throws Exception {
-        int page = 1;
-        int elementsPerPage = 10;
-        final List<Repository> fakeRepoList = (createFakeRepositoryList());
-        final RepoRepository.GetReposListener listener = mock(RepoRepository.GetReposListener.class);
-
         when(mRepositoryDao.getRepositoryCount()).thenReturn(21);
-        when(mRepositoryDao.getRepositories(page, elementsPerPage)).thenReturn(fakeRepoList);
 
         mRepoRepositoryImpl.getRepos(1, 10, listener);
 
         verify(mRepositoryDao, times(1)).getRepositoryCount();
-        verify(mRepositoryDao, times(1)).getRepositories(page, elementsPerPage);
+        verify(mRepositoryDao, times(1))
+                .getRepositories(page, elementsPerPage);
+        verifyNoMoreInteractions(mRepositoryDao);
+        verifyNoMoreInteractions(mRepositoryService);
+    }
+
+    @Test
+    public void shouldLoadRemoteRepos() throws Exception {
+        when(mRepositoryDao.getRepositoryCount()).thenReturn(0);
+
+        mRepoRepositoryImpl.getRepos(page, elementsPerPage, listener);
+
+        verify(mRepositoryDao, times(1)).getRepositoryCount();
+        verify(mRepositoryService, times(1))
+                .getRepositories(eq(page), eq(elementsPerPage),
+                        Mockito.any(RepositoryService.OnFetchCompletedListener.class));
+        verifyNoMoreInteractions(listener);
+        verifyNoMoreInteractions(mRepositoryDao);
+        verifyNoMoreInteractions(mRepositoryService);
+    }
+
+    @Test
+    public void properlyLoadLocalRepos() throws Exception {
+        final List<Repository> fakeRepoList = (createFakeRepositoryList());
+
+        when(mRepositoryDao.getRepositoryCount()).thenReturn(21);
+        when(mRepositoryDao.getRepositories(page, elementsPerPage)).thenReturn(fakeRepoList);
+
+        mRepoRepositoryImpl.getRepos(page, elementsPerPage, listener);
+
         verify(listener, times(1)).onReposLoaded(fakeRepoList);
         verifyNoMoreInteractions(listener);
-        verifyNoMoreInteractions(mRepositoryService);
+    }
+
+    @Test
+    public void properlyLoadRemoteRepos() throws Exception {
+        
+    }
+
+    @Test
+    public void shouldNotifyLoadError() throws Exception {
 
     }
 
